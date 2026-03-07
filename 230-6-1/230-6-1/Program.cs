3d7e4c886ca06271665d47d34ae1c12f7ded3375
rubyara19 <rubyara19@gmail.com>
2026-03-07 12:18:31 -0600

    update

--- 230-6-1/230-6-1/Program.cs
+++ 230-6-1/230-6-1/Program.cs
@@ -10,9 +10,9 @@
 
             // Base class Employee with id num, first name, last name
             // as well as a default constructor, set-all constructor, get and set methods for each data member
-            private int id_num;
-            private string first_name;
-            private string last_name;
+            public int id_num;
+            public string first_name;
+            public string last_name;
 
             public Employee()
             {
@@ -69,7 +69,56 @@
             }
             // display data method for Employee
 
+            public string displayEmployeeData()
+            {
+                return id_num.ToString() + " " + first_name + " " + last_name;
+            }
+
             // earnings for Employee
+        }
+
+        public class SalaryWorker : Employee
+        {
+            // salary data
+            private float salary;   
+            // default
+            public SalaryWorker() : base()
+            {
+                this.salary = 0;
+            }
+            // set all
+
+            public SalaryWorker(int id_num, string first_name, string last_name, float salary) : 
+                base(id_num, first_name, last_name)
+            {
+                this.salary = salary;
+            }
+            // set data
+            public void setSalaryworker(int Id_num = 0, string First_name = "No Label", string Last_name = "No Label",
+                float Salary = 0)
+            {
+                this.id_num = Id_num;
+                this.first_name = First_name;
+                this.last_name = Last_name;
+                this.salary = Salary;
+            }
+            // set salary
+            public float getSalary()
+            {
+                return this.salary;
+            }
+            // get salary
+            public void setSalary(float salary)
+            {
+                this.salary = salary;
+            }
+            // display data
+            public override string displayEmployeeData()
+            {
+                return base.displayEmployeeData() + " " + salary.ToString();
+            }
+            // earnings
+            
 
 
         }
